889c013ea7a06723b8b311e4e67c0d2e72591d52
abtpeb <abtpeb3827@gmail.com>
2019-06-09 19:01:33 +0900

    17. Denote Multiple items by renderitem () and map

--- App.js
+++ App.js
@@ -1,23 +1,96 @@
 import React from 'react';
-import { Image, StyleSheet, Text, ScrollView } from 'react-native';
+import { Image, StyleSheet, Text, ScrollView, View } from 'react-native';
 import { Constants } from 'expo';
 
 export default class App extends React.Component{
+  fruits = [
+    { name: 'Acai' },
+    { name: 'Apples' },
+    { name: 'Apricots' },
+    { name: 'Avocado' },
+    { name: 'Ackee' },
+    { name: 'Bananas' },
+    { name: 'Bilberries' },
+    { name: 'Blueberries' },
+    { name: 'Blackberries' },
+    { name: 'Boysenberries' },
+    { name: 'Bread fruit' },
+    { name: 'Cantaloupes (cantalope)' },
+    { name: 'Chocolate-Fruit' },
+    { name: 'Cherimoya' },
+    { name: 'Cherries' },
+    { name: 'Cranberries' },
+    { name: 'Cucumbers' },
+    { name: 'Currants' },
+    { name: 'Dates' },
+    { name: 'Durian' },
+    { name: 'Eggplant' },
+    { name: 'Elderberries' },
+    { name: 'Figs' },
+    { name: 'Gooseberries' },
+    { name: 'Grapes' },
+    { name: 'Grapefruit' },
+    { name: 'Guava' },
+    { name: 'Honeydew melons' },
+    { name: 'Horned melon (Kiwano)' },
+    { name: 'Huckleberries' },
+    { name: 'Ita Palm' },
+    { name: 'Jujubes' },
+    { name: 'Kiwis' },
+    { name: 'Durian is an unusual tropical fruit.' },
+    { name: 'Kumquat' },
+    { name: 'Lemons' },
+    { name: 'Limes' },
+    { name: 'Lychees' },
+    { name: 'Mangos' },
+    { name: 'Mangosteen' },
+    { name: 'Mulberries' },
+    { name: 'Muskmelon' },
+    { name: 'Nectarines' },
+    { name: 'Ogden melons' },
+    { name: 'Olives' },
+    { name: 'Oranges' },
+    { name: 'Papaya' },
+    { name: 'Passion fruit' },
+    { name: 'Peaches' },
+    { name: 'Pears' },
+    { name: 'Peppers' },
+    { name: 'Persimmon' },
+    { name: 'Pineapple' },
+    { name: 'Plums' },
+    { name: 'Pluot' },
+    { name: 'Pomegranate' },
+    { name: 'Prickly Pear' },
+    { name: 'Quince' },
+    { name: 'Rambuton' },
+    { name: 'Raspberries' },
+    { name: 'Rose Apple' },
+    { name: 'Starfruit' },
+    { name: 'Sapadilla' },
+    { name: 'Strawberries' },
+    { name: 'Tamarind' },
+    { name: 'Tangelo' },
+    { name: 'Tangerines' },
+    { name: 'Tomatoes' },
+    { name: 'Ugli fruit' },
+    { name: 'Voavanga (Spanish Tamarind)' },
+    { name: 'Watermelons' },
+    { name: 'Xigua melon' },
+    { name: 'Yellow watermelon' },
+    { name: 'Zucchini' },
+  ];
+
+  renderItem({ name }) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.text}>{name}</Text>
+      </View>
+    );
+  }
     render() {
         return (
             <ScrollView style={styles.container}>
-               <Text style={{fontSize:96}}>Scroll me plz</Text>
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-               <Text style={{fontSize:96}}>Scroll me plz</Text>
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
-               <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+                {this.fruits.map(this.renderItem)}
             </ScrollView>
         );
     }
@@ -28,7 +101,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     marginTop: Constants.statusBarHeight,
-    borderColor: 'red',
-    borderWidth: 2,
+    borderWidth: 1,
+    borderColor: 'orange',
   },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+  }
 });
